d644e4f3767011cd98ec54c34c86b815f25afe01
gerdb <gerd@aline-gerd.de>
2013-11-10 14:30:53 +0000

    Demo dummy functionality

--- psu-kit/main.c
+++ psu-kit/main.c
@@ -6,3 +6,38 @@
  */
 
 
+#include <avr/io.h>
+
+#define running  1
+
+
+void delay_ms( unsigned int ms )
+{
+    unsigned int  i;
+    unsigned int  j;
+
+    for ( i = ms;  i;  i-- )
+    {
+        for ( j = 51;  j;  j-- )
+        {
+        }
+    }
+}
+
+int main( void )
+{
+    // PortD6 as output
+    DDRD |= _BV(PD6);
+
+    // main loop
+    while ( running )
+    {
+    	// LED on
+        PORTD |= _BV(PD6);
+        delay_ms( 1000 );
+
+    	// LED off
+        PORTD &= ~_BV(PD6);
+        delay_ms( 29000 );
+    }
+}
