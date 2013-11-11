534cc4fb9f87b230636ea61db5788dcb97cd6acd
gerd@aline-gerd.de <gerd@aline-gerd.de@a00bf1b6-e57a-9883-da5f-c57ba6cdb29b>
2013-11-11 21:01:41 +0000

    50% PWM with 31kHz at OC1A

---
+++ trunk/psu-kit/inc/pwm.h
@@ -0,0 +1,31 @@
+/*
+ *  Project:      psu-kit
+ *  File:         pwm.c
+ *  Author:       gerd bartelt - www.sebulli.com
+ *
+ *  Description:  header file for pwm.c
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#ifndef PWM_H_
+#define PWM_H_
+
+//Function prototypes
+void PWM_Init(void);
+
+
+#endif /* PWM_H_ */
